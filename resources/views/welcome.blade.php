2f5ebebfb974f5db4030d8406f88d46538813d00
Nicola Rizzitelli <nicola.rizzitelli20@gmail.com>
2023-03-03 17:05:24 +0100

    sito pannelli solari

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,56 @@
+<x-layout>
+
+    <div class="container-fluid text-center">
+        <div class="row">
+            <div class="col-12">
+                <img src="https://www.infobuildenergia.it/wp-content/uploads/2020/02/posti-lavoro-fotovoltaico-1024x535.jpg" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="container my5 text-center">
+        <div class="row justify-content-center">
+            <div class="col-12">
+                <h1>How is solar panel work? 
+                </h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas distinctio laboriosam voluptas quidem at ullam nam nesciunt aperiam in reiciendis qui, est officia, sapiente culpa illo fugiat. Autem, ad ipsum!</p>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 col-md-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                      <img src="https://img.icons8.com/ios/50/null/solar-panel.png"/>
+                      <h5 class="card-title">INVERTER INSPECTION</h5>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-12 col-md-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                      <img src="https://img.icons8.com/ios/50/null/solar-energy.png"/>
+                      <h5 class="card-title">COMPLETE DIAGNOSTICS</h5>
+                    </div>
+                  </div>
+            </div>
+            <div class="col-12 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                          <img src="https://img.icons8.com/ios/50/null/solar-panel.png"/>
+                          <h5 class="card-title">EXPERT SOLAR PANEL REPAIR</h5>
+                        </div>
+                    </div>
+            </div>
+            <div class="col-12 col-md-3">
+                    <div class="card text-center">
+                        <div class="card-body">
+                          <img src="https://img.icons8.com/ios/50/null/solar-energy.png"/>
+                          <h5 class="card-title">UPDATES & MONITORING</h5>
+                        </div>
+                      </div>
+            </div>
+        </div>
+    </div>
+
+</x-layout>
---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PublicController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+Route::get('/contact-us', [PublicController::class, 'contact_us'])->name('contact_us');
+Route::post('/contact-us/submit', [PublicController::class, 'contact_us_submit'])->name('contact_us_submit');
